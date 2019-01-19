98b6d0849445965eb5e31a80cf989e1875000d42
boredelf <shinobies@gmail.com>
2019-01-19 03:19:19 -0200

    Normalized identation.

--- script.sh
+++ script.sh
@@ -5,26 +5,26 @@ expirationLabel='expiresAt'
 echo -n "Looking for namespaces labeled with \"${expirationLabel}\"... "
 namespaces=($(kubectl get ns -l ${expirationLabel} 2> /dev/null | tail -n +2 | awk '{print $1}'))
 if [[ ${#namespaces[@]} -eq 0 ]];
-	then echo 'no labeled namespaces found.'
-	else echo "${#namespaces[@]} labeled namespace(s) found."
+   then echo 'no labeled namespaces found.'
+   else echo "${#namespaces[@]} labeled namespace(s) found."
 fi
 
 today=$(date +%Y%m%d)
 expiredNamespaces=()
 echo -n 'Looking for expired ones... '
 for ns in ${namespaces[@]}; do
-    expirationDate=$(kubectl get ns ${ns} --template='{{.metadata.labels.expiresAt}}' | tr -d '-')
-    if [[ ${expirationDate} -le ${today} ]]; then expiredNamespaces+=(${ns}); fi
+   expirationDate=$(kubectl get ns ${ns} --template='{{.metadata.labels.expiresAt}}' | tr -d '-')
+   if [[ ${expirationDate} -le ${today} ]]; then expiredNamespaces+=(${ns}); fi
 done
 
 echo "found ${#expiredNamespaces[@]} expired namespace(s)."
 for ens in ${expiredNamespaces[@]}; do
-	deleteSuccessful=$(kubectl delete ns ${ens} 2>&1)
-	echo -n "Deleting \"${ens}\"... "
-	if [[ ! ${deleteSuccessful} =~ ^Error ]];
-		then echo 'deleted.'
-		else echo "not deleted. Stderr: ${deleteSuccessful}"
-	fi
+   deleteSuccessful=$(kubectl delete ns ${ens} 2>&1)
+   echo -n "Deleting \"${ens}\"... "
+   if [[ ! ${deleteSuccessful} =~ ^Error ]];
+      then echo 'deleted.'
+      else echo "not deleted. Stderr: ${deleteSuccessful}"
+   fi
 done
 
 # TODO: Delete Docker images (using branch's name, extracted from namespace) from Nexus?
