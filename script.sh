db22a3b8d6d957cc5d3b17154d20e5d60ba2b93f
boredelf <shinobies@gmail.com>
2019-01-20 21:56:49 -0200

    Added cluster connection check.

--- script.sh
+++ script.sh
@@ -2,10 +2,17 @@
 
 expirationLabel='expiresAt'
 
+echo 'Checking cluster connection...'
+clusterConnectionCheck=$(kubectl cluster-info 2>&1 1>/dev/null)
+if [[ -n ${clusterConnectionCheck} ]]; then
+   echo "Aparently there's an error: ${clusterConnectionCheck}"
+   echo 'Terminating the script now.' && exit 1
+fi
+
 echo -n "Looking for namespaces labeled with \"${expirationLabel}\"... "
 namespaces=($(kubectl get ns -l ${expirationLabel} 2> /dev/null | tail -n +2 | awk '{print $1}'))
 if [[ ${#namespaces[@]} -eq 0 ]];
-   then echo 'no labeled namespaces found.'
+   then printf 'no labeled namespaces found.\nNothing to do. Terminating the script now.' && exit
    else echo "${#namespaces[@]} labeled namespace(s) found."
 fi
 
@@ -14,7 +21,9 @@ expiredNamespaces=()
 echo -n 'Looking for expired ones... '
 for ns in ${namespaces[@]}; do
    expirationDate=$(kubectl get ns ${ns} --template='{{.metadata.labels.expiresAt}}' | tr -d '-')
-   if [[ ${expirationDate} -le ${today} ]]; then expiredNamespaces+=(${ns}); fi
+   if [[ ${expirationDate} -le ${today} ]];
+      then expiredNamespaces+=(${ns});
+   fi
 done
 echo "found ${#expiredNamespaces[@]} expired namespace(s)."
 
